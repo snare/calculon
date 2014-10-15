6b6d10cd1892e4d1c964b8191776b311f35b981e
snare <snare@ho.ax>
2014-10-15 23:44:07 +1100

    Updated voltron support for lionforce

--- calculon/voltron_integration.py
+++ calculon/voltron_integration.py
@@ -5,49 +5,60 @@ from .env import *
 
 try:
     import voltron
+    from voltron.core import *
+    from voltron.api import *
+    from voltron.plugin import *
+    voltron.setup_env()
 except ImportError:
     voltron = None
 
 class VoltronProxy(object):
     _instance = None
-    config = {
-        'type': 'interactive',
-        'update_on': 'interactive',
-    }
+    exception = False
 
     def __new__(cls, *args, **kwargs):
         if voltron:
             if not cls._instance:
-                cls._instance = super(VoltronProxy, cls).__new__(cls, *args, **kwargs)                
+                cls._instance = super(VoltronProxy, cls).__new__(cls, *args, **kwargs)
+                cls._instance.connected = False
+                cls._instance.connect()
         else:
             cls._instance = None
         return cls._instance
 
-    def __init__(self):
-        self.connected = False
-        self.connect()
-
     def __getattr__(self, key):
         if self.connected:
-            resp = self.client.query({'msg_type': 'interactive',
-                              'query': 'get_register',
-                              'register': key})
-            return resp['value']
+            try:
+                req = api_request('read_registers')
+                res = self.client.send_request(req)
+                if res.status == 'success':
+                    return res.registers[key]
+                else:
+                    print("{} getting register: {}".format(type(res), res.message))
+            except Exception as e:
+                print("Exception getting register: {} {}".format(type(e), str(e)))
         else:
             raise Exception("Not connected")
 
     def __getitem__(self, key):
         if self.connected:
-            if isinstance(key, slice):
-                resp = self.client.query({'msg_type': 'interactive',
-                    'query': 'get_memory',
-                    'start': key.start,
-                    "end": key.stop})
-            else:
-                resp = self.client.query({'msg_type': 'interactive',
-                    'query': 'get_memory',
-                    'start': key,
-                    "end": key + 1})
+            try:
+                req = api_request('read_memory')
+                if isinstance(key, slice):
+                    req.address = key.start
+                    req.length = key.stop - key.start
+                else:
+                    req.address = key
+                    req.length = 1
+
+                res = self.client.send_request(req)
+
+                if res.status == 'success':
+                    return res.memory
+                else:
+                    print("{} reading memory: {}".format(type(res), res.message))
+            except Exception as e:
+                print("Exception reading memory: {} {}".format(type(e), str(e)))
 
             return resp['value']
         else:
@@ -55,15 +66,18 @@ class VoltronProxy(object):
 
     def connect(self):
         if not self.connected:
-            try:
-                self.client = voltron.comms.InteractiveClient(config=self.config)
-                self.connected = True
-                print("Connected to voltron")
-            except socket.error:
-                pass
-            except Exception as e:
-                print("Error loading voltron: " + str(e))
-                print("Make sure you have the most recent version of voltron")
+            if not self.exception:
+                try:
+                    self.client = voltron.core.Client()
+                    self.client.connect()
+                    self.connected = True
+                    print("Connected to voltron")
+                except socket.error:
+                    pass
+                except Exception as e:
+                    self.exception = True
+                    print("Error loading voltron: " + str(e))
+                    print("Make sure you have the most recent version of voltron")
         else:
             print("Already connected")
 
