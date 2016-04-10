5e3d0ce24740a423b0476ae330644f00a3eac154
snare <snare@ho.ax>
2016-04-10 17:46:58 +1000

    PEP8

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup
 
 setup(
-    name = "calculon",
-    version = "0.1",
-    author = "snare",
-    author_email = "snare@ho.ax",
-    description = ("A terminal-based programmer's calculator endowed with unholy acting talent by the Robot Devil"),
-    license = "Buy snare a beer",
-    keywords = "calculator programmer hex 64-bit",
-    url = "https://github.com/snarez/calculon",
+    name="calculon",
+    version="0.1",
+    author="snare",
+    author_email="snare@ho.ax",
+    description=("A terminal-based programmer's calculator endowed with unholy acting talent by the Robot Devil"),
+    license="Buy snare a beer",
+    keywords="calculator programmer hex 64-bit",
+    url="https://github.com/snarez/calculon",
     packages=['calculon'],
-    install_requires = ['rl', 'Pyro4', 'blessings', 'scruffington'],
-    package_data = {'calculon': ['config/*']},
-    install_package_data = True,
-    entry_points = {'console_scripts': ['calculon = calculon:main']},
+    install_requires=['rl', 'Pyro4', 'blessings', 'scruffington'],
+    package_data={'calculon': ['config/*']},
+    entry_points={'console_scripts': ['calculon=calculon:main']},
     zip_safe=False
 )
